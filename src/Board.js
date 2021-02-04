5f6698a0e379f3b700d1f7e709929cf62ff3c911
Camila Daniels <camila.daniels926@gmail.com>
2021-02-04 17:57:07 -0400

    Defined onClick event handles for each Cell component. The event handlers toggles between X and O and displays the character on the board. Next, I will define the logic that will score each individual board.

--- src/Board.js
+++ src/Board.js
@@ -5,20 +5,69 @@ import './Board.css'
 class Board extends Component {
 
 	static defaultProps = {
-		BOARD_SIZE: 9
+		BOARD_SIZE: 9,
+		N_ROWS: 3,
+		N_COLS: 3
 	}
 
 	constructor(props) {
 		super(props)
+
+		this.state = {
+			board: this.createBoard(),
+			currPlayer: "X"
+		}
 	}
+
+	markCell(i, j) {
+
+		let board = this.state.board;
+
+		if (j >= 0 && j < this.props.N_COLS && i >= 0 && i < this.props.N_ROWS) {
+			board[i][j] = this.state.currPlayer
+		}
+
+		this.setState({
+			board: board
+		})
+
+		this.togglePlayer()
+	}
+
+	togglePlayer() {
+		(this.state.currPlayer === "X" ? 
+				this.setState({
+					currPlayer: "O"
+				}) : 
+				this.setState({
+					currPlayer: "X"
+				})
+		)
+	}
+
+	createBoard() {
+		let board = [];
+
+		for (let i = 0; i < this.props.N_ROWS; i++) {
+			let row = [];
+			board.push(row);
+		} 
+
+		return board;
+	}
+
 	render() {
 
 		let cells = [];
 
-		for (let i = 0; i < this.props.BOARD_SIZE; i++) {
-			cells.push(
-				<Cell />
-			)
+		for (let i = 0; i < this.props.N_ROWS; i++) {
+			let row = [];
+			for (let j = 0; j < this.props.N_COLS; j++) {
+				row.push(
+					<Cell value={this.state.board[i][j]} showValue={() => this.markCell(i, j)}/>
+				)
+			}
+			cells.push(<div>{row}</div>)
 		}
 
 		return(
--- src/Cell.js
+++ src/Cell.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './Cell.css';
 
 class Cell extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.handleClick = this.handleClick.bind(this)
+	} 
+
+
+	handleClick(event) {
+		this.props.showValue()
+	}
+
 	render() {
 		return(
-			<div className="Cell">
-				
+			<div className="Cell" onClick={this.handleClick}>
+				{this.props.value}
 			</div>
 		)
 	}
