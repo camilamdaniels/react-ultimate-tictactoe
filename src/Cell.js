c50e79767c6c25c7d16fc599efa0883c2bcedb75
Camila Daniels <camila.daniels926@gmail.com>
2021-02-04 17:15:53 -0400

    Modified styling so that the board looks more like an actual game board.

--- src/Cell.js
+++ src/Cell.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './Cell.css';
 
 class Cell extends Component {
 	render() {
 		return(
-			<div>
-				Cell
+			<div className="Cell">
+				
 			</div>
 		)
 	}
